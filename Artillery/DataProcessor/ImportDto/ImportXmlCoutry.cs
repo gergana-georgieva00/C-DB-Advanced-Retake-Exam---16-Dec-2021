0b10b1691ce79926fe6647ffdfcf6b80158d686f
Gergana Georgieva <gerganagergana1@gmail.com>
2023-03-31 12:23:49 +0300

    Ex 2

--- Artillery/DataProcessor/ImportDto/ImportXmlCoutry.cs
+++ Artillery/DataProcessor/ImportDto/ImportXmlCoutry.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Artillery.DataProcessor.ImportDto
 {
-    internal class ImportXmlCoutry
+    [XmlType("Country")]
+    public class ImportXmlCoutry
     {
+        [XmlElement("CountryName")]
+        [Required, MinLength(4), MaxLength(60)]
+        public string CountryName { get; set; }
+
+        [XmlElement("ArmySize")]
+        [Required, Range(50000, 10000000)]
+        public int ArmySize { get; set; }
     }
 }
