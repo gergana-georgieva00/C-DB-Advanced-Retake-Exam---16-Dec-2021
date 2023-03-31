14da98fc9d39da2fb430611c4b04d0640d96f665
Gergana Georgieva <gerganagergana1@gmail.com>
2023-03-31 12:30:30 +0300

    Ex 2

--- Artillery/DataProcessor/Deserializer.cs
+++ Artillery/DataProcessor/Deserializer.cs
@@ -1,7 +1,11 @@
 ﻿namespace Artillery.DataProcessor
 {
     using Artillery.Data;
+    using Artillery.Data.Models;
+    using Artillery.DataProcessor.ImportDto;
     using System.ComponentModel.DataAnnotations;
+    using System.Text;
+    using System.Xml.Serialization;
 
     public class Deserializer
     {
@@ -18,7 +22,32 @@
 
         public static string ImportCountries(ArtilleryContext context, string xmlString)
         {
-            throw new NotImplementedException();
+            var countryDtos = Deserialize<ImportXmlCoutry[]>(xmlString, "Countries");
+            var sb = new StringBuilder();
+            var countries = new List<Country>();
+
+            foreach (var dto in countryDtos)
+            {
+                if (!IsValid(dto))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;   
+                }
+
+                var country = new Country()
+                {
+                    CountryName = dto.CountryName,
+                    ArmySize = dto.ArmySize
+                };
+
+                countries.Add(country);
+                sb.AppendLine(String.Format(SuccessfulImportCountry, dto.CountryName, dto.ArmySize));
+            }
+
+            context.AddRange(countries);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
         }
 
         public static string ImportManufacturers(ArtilleryContext context, string xmlString)
@@ -35,6 +64,18 @@
         {
             throw new NotImplementedException();
         }
+
+        private static T Deserialize<T>(string inputXml, string rootName)
+        {
+            var root = new XmlRootAttribute(rootName);
+            var serializer = new XmlSerializer(typeof(T), root);
+
+            using StringReader reader = new StringReader(inputXml);
+
+            T dtos = (T)serializer.Deserialize(reader);
+            return dtos;
+        }
+
         private static bool IsValid(object obj)
         {
             var validator = new ValidationContext(obj);
